745db8ee9c66a8f918ade8b9e0c5c771432395d6
[danceforever] <[baranyi.eszti@gmail.com]>
2025-08-21 21:32:26 +0200

    valtozas

--- vizsgamunka-baranyi-eszter/js/main.js
+++ vizsgamunka-baranyi-eszter/js/main.js
@@ -135,27 +135,3 @@ container.addEventListener('click', () => {
 });
 
 
-//lightbox
-const lightbox = document.querySelector('.lightbox');
-const lightboxImg = document.querySelector('.lightbox-img');
-const closeBtn = document.querySelector('.lightbox .close');
-
-document.querySelectorAll('.thumbnails img').forEach(img => {
-    img.addEventListener('click', () => {
-        lightbox.style.display = 'flex';
-        lightboxImg.src = img.src;
-        lightboxImg.alt = img.alt;
-    });
-});
-
-closeBtn.addEventListener('click', () => {
-    lightbox.style.display = 'none';
-    lightboxImg.src = "";
-});
-
-lightbox.addEventListener('click', (e) => {
-    if (e.target === lightbox) {
-        lightbox.style.display = 'none';
-        lightboxImg.src = "";
-    }
-});
